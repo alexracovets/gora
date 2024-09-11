ff55b9e7e5e62e8b02261b5ca94d39b4e10e5b74
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-09-11 10:19:36 +0300

    add

--- src/Components/GiftSection/GiftSection.jsx
+++ src/Components/GiftSection/GiftSection.jsx
@@ -4,6 +4,7 @@ import Container from "../Container/Container";
 import Title from "../UI/Title/Title";
 
 import s from './GiftSection.module.scss';
+import DashLine from "../UI/DashLine/DashLine";
 export default function GiftSection() {
     const { t } = useTranslation();
 
@@ -11,7 +12,23 @@ export default function GiftSection() {
         <section className={s.gift}>
             <Container>
                 <Title>{t("title.gifts")}</Title>
-
+                <div className={s.line_wrapper}>
+                    <DashLine />
+                    <p className={s.text}>{t("dash_text.huge_patron")}</p>
+                </div>
+                <div className={s.slider}>
+                    <div className={s.item}>
+                        <div className={s.item_wrapper}>
+                            <div className={s.name}>
+                                Покровительство
+                            </div>
+                            <div className={s.price}>
+                                за <span>€500.000</span>
+                            </div>
+                            <button> зробити внесок  </button>
+                        </div>
+                    </div>
+                </div>
             </Container>
         </section>
     )
