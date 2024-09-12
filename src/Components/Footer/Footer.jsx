e76e2d6c216469b81a9ae2ea21b32d84fdb0b6f0
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-09-12 07:40:03 +0300

    add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
+import { useTranslation } from "react-i18next";
+
 import Container from "../Container/Container";
+import Button from "../UI/Button/Button";
 
 import s from './Footer.module.scss';
 export default function Footer() {
+    const { t } = useTranslation();
 
     return (
         <footer>
@@ -18,12 +22,29 @@ export default function Footer() {
                         </div>
                         <div className={s.links_icons}>
                             <a className={s.item}>
-                                <img src="./img/social/facebook.svg" alt="" />
+                                <img src="./img/social/facebook.svg" alt="facebook" />
                             </a>
                             <a className={s.item}>
-                                <img src="./img/social/instagram.svg" alt="" />
+                                <img src="./img/social/instagram.svg" alt="instagram" />
                             </a>
                         </div>
+                        <a href="www.pilgrimage.com.ua" className={s.site_link}>www.pilgrimage.com.ua</a>
+                        <div className={s.link_info}>
+                            Посилання на сторінку о. Паїсія у фейсбуці, де регулярно публікуватиметься інформація про стан робіт, виконаних у Келії
+                        </div>
+                        <div className={s.links_icons}>
+                            <a className={s.item}>
+                                <img src="./img/social/facebook.svg" alt="facebook" />
+                            </a>
+                        </div>
+                        <a href="https://wa.me/380503525236" className={s.site_link}>
+                            +380503525236 <br />
+                            (WhatsApp, Signal)
+                        </a>
+                        <a href="mailto:pilgrimage_center@ukr.net" className={s.site_link}>
+                            pilgrimage_center@ukr.net
+                        </a>
+                        <Button>{t("btn.tel")}</Button>
                     </div>
                 </div>
             </Container>
