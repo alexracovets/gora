475b299a2a56761a1b4ad13ac37399e0cc665dc3
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-09-12 08:26:40 +0300

    add

--- src/Components/GoogleMap/GoogleMap.jsx
+++ src/Components/GoogleMap/GoogleMap.jsx
@@ -1,13 +1,5 @@
-import GoogleMapReact from "google-map-react";
-
 export default function GoogleMap() {
-    const defaultProps = {
-        center: {
-            lat: 10.99835602,
-            lng: 77.01502627
-        },
-        zoom: 11
-    };
+
     return (
 
         <>
