69002cb1b419cc617350d58bb27b8e881c20478f
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-09-12 07:16:07 +0300

    add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,26 +5,28 @@ export default function Footer() {
 
     return (
         <Container>
-            <section>
+            <footer className={s.footer}>
                 <div className={s.contacts}>
                     <div className={s.title}>
                         Контакти
                     </div>
                     <div className={s.cosial}>
                         <div className={s.name}>
-                            Соцмережі Паломницького Центру
+                            Соцмережі
+                            <br />
+                            Паломницького Центру
                         </div>
                         <div className={s.links_icons}>
-                            <div className={s.item}>
-                                <img src="" alt="" />
-                            </div>
-                            <div className={s.item}>
-                                <img src="" alt="" />
-                            </div>
+                            <a className={s.item}>
+                                <img src="./img/social/facebook.svg" alt="" />
+                            </a>
+                            <a className={s.item}>
+                                <img src="./img/social/instagram.svg" alt="" />
+                            </a>
                         </div>
                     </div>
                 </div>
-            </section>
+            </footer>
         </Container>
     )
 }
