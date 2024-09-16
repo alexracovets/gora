68e26e52a33f0e09548979d29ab2393967caf521
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-09-16 13:29:20 +0300

    fixed text

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -24,28 +24,28 @@ export default function Footer() {
                                 Паломницького Центру
                             </div>
                             <div className={s.links_icons}>
-                                <a className={s.item}>
+                                {/* <a className={s.item} target="_blank" rel="noreferrer">
                                     <img src="./img/social/facebook.svg" alt="facebook" />
-                                </a>
-                                <a className={s.item}>
+                                </a> */}
+                                <a className={s.item} href="https://www.instagram.com/pilgrimagecenter/" target="_blank" rel="noreferrer">
                                     <img src="./img/social/instagram.svg" alt="instagram" />
                                 </a>
                             </div>
-                            <a href="www.pilgrimage.com.ua" className={s.site_link}>www.pilgrimage.com.ua</a>
+                            <a href="https://www.pilgrimage.com.ua/" className={s.site_link} target="_blank" rel="noreferrer">www.pilgrimage.com.ua</a>
                             <div className={s.link_info}>
                                 Посилання на сторінку о. Паїсія у фейсбуці, де регулярно публікуватиметься інформація про стан робіт, виконаних у Келії
                             </div>
                             <div className={s.links_icons}>
-                                <a className={s.item}>
+                                <a className={s.item} href="https://www.facebook.com/profile.php?id=100083772765560" target="_blank" rel="noreferrer">
                                     <img src="./img/social/facebook.svg" alt="facebook" />
                                 </a>
                             </div>
                             <div className={s.links_wrapper}>
-                                <a href="https://wa.me/380503525236" className={s.site_link}>
+                                <a href="https://wa.me/380503525236" className={s.site_link} target="_blank" rel="noreferrer">
                                     +380503525236 <br />
                                     (WhatsApp, Signal)
                                 </a>
-                                <a href="mailto:pilgrimage_center@ukr.net" className={s.site_link}>
+                                <a href="mailto:pilgrimage_center@ukr.net" className={s.site_link} target="_blank" rel="noreferrer">
                                     pilgrimage_center@ukr.net
                                 </a>
                             </div>
