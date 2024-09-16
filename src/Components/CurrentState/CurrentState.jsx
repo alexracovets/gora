22e1018752d5e175d908b36ed9501383f2539bf7
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-09-16 12:39:56 +0300

    added ytube

--- src/Components/CurrentState/CurrentState.jsx
+++ src/Components/CurrentState/CurrentState.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import YouTube from 'react-youtube';
 
 import Container from "../Container/Container";
 import Title from "../UI/Title/Title";
@@ -6,13 +7,27 @@ import Title from "../UI/Title/Title";
 import s from './CurrentState.module.scss';
 export default function CurrentState() {
     const { t } = useTranslation();
-
+    const opts = {
+        height: '100%',
+        width: '100%',
+        playerVars: {
+            rel: 0,
+            controls: 0,
+            disablekb: 1,
+            fs: 0,
+            start: 11
+        },
+    };
     return (
         <section className={s.state}>
             <Container>
                 <Title>{t("title.current_state")}</Title>
                 <div className={s.video_wrapper}>
-                    <video src=""></video>
+                    <YouTube
+                        videoId="_-8UIkpVZiM"
+                        opts={opts}
+                        className={s.video}
+                    />
                 </div>
             </Container>
         </section>
