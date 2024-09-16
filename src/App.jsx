ec47376dd69e9a96d8caac048c4c15f4c7543383
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-09-16 09:03:41 +0300

    fixed style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
-import ScrollToHashElement from "@cascadia-code/scroll-to-hash-element";
 
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Main from "./Components/Main/Main";
+import ScrollToHashElement from './static/ScrollToHashElement';
 
 export default function App() {
-
   return (
     <BrowserRouter>
       <ScrollToHashElement behavior="smooth" inline="start" block="start" />
