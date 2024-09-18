9b8efe2db9569d3c19800da8b0478c920b951683
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-09-18 22:12:56 +0300

    test

--- src/Components/FirstInfo/FirstInfo.jsx
+++ src/Components/FirstInfo/FirstInfo.jsx
@@ -1,16 +1,5 @@
-import { useTranslation } from "react-i18next";
-
-import Container from "../Container/Container";
-import DashLine from "../UI/DashLine/DashLine";
-import Button from "../UI/Button/Button";
-import Title from "../UI/Title/Title";
-
-import modalStore from '../../store/modalStore';
-
-import s from './FirstInfo.module.scss';
+import InfoSection from "../InfoSection/InfoSection";
 export default function FirstInfo() {
-    const { t } = useTranslation();
-    const setIsPayModal = modalStore((state) => state.setIsPayModal);
 
     const content = {
         title: "title.gathering",
@@ -26,25 +15,6 @@ export default function FirstInfo() {
     }
 
     return (
-        <section className={`${s.info}`} id={content.id}>
-            <Container>
-                <Title>{t(content.title)}</Title>
-                <div className={s.we_are}>
-                    {content.startText.map((text, idx) => {
-                        return (
-                            <p key={idx} dangerouslySetInnerHTML={{ __html: t(text) }} />
-                        )
-                    })}
-                </div>
-                <DashLine />
-                <div className={s.help}>
-                    {content.help.map((item, idx) => {
-                        return <p key={idx} dangerouslySetInnerHTML={{ __html: t(item) }} />
-                    })}
-                </div>
-                {content.btn ? <Button click={() => setIsPayModal(true)}>{t(content.btn)}</Button> : null}
-
-            </Container>
-        </section >
+        <InfoSection content={content} fullscreen />
     )
 } 
