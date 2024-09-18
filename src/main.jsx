7233cd2653499b99cc8dcc843016ca8c19d5a3b3
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-09-18 16:33:27 +0300

    fixed style

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
  
 import { I18nextProvider } from 'react-i18next';
 import ReactDOM from 'react-dom/client';
-import React from 'react';
 
 
 import App from './App.jsx';
@@ -9,9 +8,7 @@ import i18n from "./locales/i18n";
 
 import './index.scss';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <I18nextProvider i18n={i18n}>
       <App />
-    </I18nextProvider>
-  </React.StrictMode>,
+    </I18nextProvider>,
 )
