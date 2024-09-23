25d70dd2650c92199e9e2f19269076a376c7f694
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-09-23 15:25:32 +0300

    fixed style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { motion, AnimatePresence } from "framer-motion";
 import { Scrollbars } from 'react-custom-scrollbars-2';
 import { BrowserRouter } from 'react-router-dom';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import ScrollToHashElement from './static/ScrollToHashElement';
 import Header from './Components/Header/Header';
@@ -8,12 +9,34 @@ import Main from './Components/Main/Main';
 import Footer from './Components/Footer/Footer';
 import ModalPay from './Components/Modal/ModalPay/ModalPay';
 import ModalProgress from './Components/Modal/ModalProgress/ModalProgress';
+import Loader from './Components/Loader/Loader';
 
 export default function App() {
   const scrollbarsRef = useRef(null);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false)
+    }, 3000)
+  }, [])
+
 
   return (
     <BrowserRouter>
+      <AnimatePresence>
+        {
+          loader ? <motion.div className={'loader'}
+            initial={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+            exit={{ opacity: 0 }}
+          >
+            <Loader />
+          </motion.div> : null
+        }
+
+      </AnimatePresence>
+
       <ScrollToHashElement behavior="smooth" inline="start" block="start" />
       <Scrollbars
         autoHeight
