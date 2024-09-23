0c547575c8b98cb04211aaf4c5a23d3e1f028ae6
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-09-23 15:51:33 +0300

    fixed style

--- src/Components/Modal/ModalProgress/ProgressItem/VideoPlayer/VideoPlayer.jsx
+++ src/Components/Modal/ModalProgress/ProgressItem/VideoPlayer/VideoPlayer.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
 import PropTypes from 'prop-types';
 
@@ -6,34 +6,49 @@ import s from '../../ModalProgress.module.scss';
 import Loader from '../../../../Loader/Loader';
 
 export default function VideoPlayer({ src }) {
-    const [isLoading, setIsLoading] = useState(true); // Спочатку завантажуємо
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
 
-    const handleReady = () => {
-        setIsLoading(false); // Коли відео готове, вимикаємо лоудер
-    };
+    useEffect(() => {
+        // Робимо запит на завантаження відео
+        const checkVideoAvailability = async () => {
+            try {
+                const response = await fetch(src);
+                if (response.ok) {
+                    setIsLoading(false); // Якщо ресурс доступний, вимикаємо лоудер
+                } else {
+                    setIsError(true); // Якщо ресурс недоступний, показуємо помилку
+                }
+            } catch (error) {
+                setIsError(true); // Помилка при запиті
+            }
+        };
 
-    const handleError = () => {
-        setIsLoading(false); // Вимкнути лоудер у випадку помилки
-    };
+        checkVideoAvailability();
+    }, [src]);
+
+    if (isError) {
+        return <Loader />; // Показуємо лоудер або інший компонент у разі помилки
+    }
 
     return (
         <div className={s.content_wrapper}>
-            {isLoading && <Loader />} {/* Показуємо лоудер, якщо isLoading === true */}
-            <ReactPlayer
-                className={s.content_wrapper}
-                url={src}
-                playing
-                muted
-                loop
-                onReady={handleReady}  // Коли відео готове до відтворення
-                onError={handleError}  // Якщо сталася помилка
-                width="100%"
-                height="100%"
-            />
+            {isLoading ? (
+                <Loader />  // Показуємо лоудер, поки чекаємо на відповідь
+            ) : (
+                <ReactPlayer
+                    url={src}
+                    playing
+                    muted
+                    loop
+                    width="100%"
+                    height="100%"
+                />
+            )}
         </div>
     );
 }
 
 VideoPlayer.propTypes = {
-    src: PropTypes.string.isRequired
+    src: PropTypes.string.isRequired,
 };
